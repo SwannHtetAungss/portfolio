a62596a2b652b3a19da162dbef3c961f9e97fe25
SwannHtetAung <swannhtetaung.ss@gmail.com>
2021-08-31 14:02:41 +0630

    progress bar animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -226,4 +226,21 @@
     });
   }
 
+  /**
+   * Skills animation
+   */
+  let skilsContent = select('.skills-content');
+  if (skilsContent) {
+    new Waypoint({
+      element: skilsContent,
+      offset: '20%',
+      handler: function(direction) {
+        let progress = select('.progress .progress-bar', true);
+        progress.forEach((el) => {
+          el.style.width = el.getAttribute('aria-valuenow') + '%'
+        });
+      }
+    })
+  }
+
 })()
